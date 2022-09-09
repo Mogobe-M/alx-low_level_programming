234c24444be86acf9b2236d4199e0bec4369199c
Mogobe-M <moshopjadi1602@gmail.com>
2022-09-08 19:30:36 -0700

    hello, world

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,15 @@
 #include<stdio.h>
-int main()
-{
-  //string initialisation
-  char Mystr[] = "Programming is like building a multilingual puzzle";
+/**
+ *You are not allowed to use any functions listed in the NAME section of the man (3) printf or man (3) puts
+ *Your program should return 1
+ *Your program should compile without any warnings when using the -Wall gcc option
 
-  puts(Mystr); //writing the string to stdout
+ */
+
+int main(void)
+{
+
+  puts("\"Programming is like building a multilingual puzzle");
 
   return 0;
 }
