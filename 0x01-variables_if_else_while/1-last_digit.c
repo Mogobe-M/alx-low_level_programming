b6f7e17da821a02766c66e746d85be3e421deb84
Mogobe-M <moshopjadi1602@gmail.com>
2022-09-11 06:10:03 -0700

    if, else, while, variables

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -1,30 +1,31 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-/**
- *main-print if the number is greater or lesser
-*discription:using the main function
-*this program prints 'programing is ositive, zero or negative'
-*returns 0
-**/
-int main(void)
-{
-  int n;
-  int l;
-  srand(time(0))
-  l = n % 10
+ /**
+  *Main- print is the number is positive, zero or negative
+  *Discription: using the main function
+  *This program prints ""programming is positive, zero or negative
+  *Return 0
+  **/
 
-    if (l > 5)
-      {
-       print f("last digit of %d is %d and is greater than 5\n", n, l);
-      }
-    else if (l ==0)
-      {
-       printf("last digit of %d is %d and is 0\n", n, l)
-      }
-    else (n < 6)
-	   {
-	    printf("last digit of %d is %d and is less than 6\n", n, l);
-	   }
-  return (0);
+int main(void){
+	int n;
+
+	srand(time(0));
+	n = srand() - RAND_MAX / 2;
+	/*source code goes here*/
+
+	if (n > 0)
+	  {
+	    printf("Last digit of n is %d greater than 5 \n", n);
+	  }
+	else if (n == 0)
+	  {
+	    printf("Last digit of n is %d is zero\n", n);
+	  }
+	else if (n != 0, n > 6)
+	  {
+	    printf("Last digit of n is %d and is less than 6 and not 0\n", n);
+	  }
+	return(0);
 }
---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ *Main- print lowercase alphabets
+ *Discription: using the main function
+ *This program prints "lowercase alphabets"
+ *Return 0
+ **/
+
+int main(void)
+{
+  
+	char ch = 'a';
+	while (ch <= 'z')
+	{
+	  printf("%c", ch);
+	}
+	return(0);
+}
