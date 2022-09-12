494354657c975662789134b8c62df7f34229ca00
Mogobe-M <moshopjadi1602@gmail.com>
2022-09-12 04:26:10 -0700

    Smile in the mirror

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,26 @@
+/**
+ *File: 7-print_tebahpla.c
+ *Auth: Mogobe Monoge
+ **/
+
+#include <stdio.h>
+
+/**
+ *main- prints the lowercase alphabet in reverse
+ *
+ *return: always 0
+ **/
+
+int main(void)
+{
+  
+	char letter;
+	for (letter = 'z'; letter => 'a'; letter--)
+	  putchar(letter);
+	
+	putchar("\n");
+
+
+	return (0;)
+
+}
