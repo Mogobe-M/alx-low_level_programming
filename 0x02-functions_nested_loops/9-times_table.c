0a924a54f5724e292fb26b0fde7190f1c104b13f
Mogobe-M <moshopjadi1602@gmail.com>
2022-09-13 07:41:53 -0700

    functions and nested loops

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,35 @@
+/**
+ * File: 9-times_table.c
+ * Auth: Monoge M Mogobe
+ **/
+
+#include "stdio.h"
+
+/**
+ * times_table - Prints the 9 times table, starting with 0.
+ */
+void times_table(void)
+{
+	int num, mult, prod;
+
+	for (num = 0; num <= 9; num++)
+	{
+		putchar('0');
+
+		for (mult = 1; mult <= 9; mult++)
+		{
+			putchar(',');
+			putchar(' ');
+
+			prod = num * mult;
+
+			if (prod <= 9)
+				putchar(' ');
+			else
+				putchar((prod / 10) + '0');
+
+			putchar((prod % 10) + '0');
+		}
+		putchar('\n');
+	}
+}
