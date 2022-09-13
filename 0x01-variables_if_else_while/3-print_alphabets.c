850d9dbf245ad52cd68afb83a2aa4def233dd0bf
Mogobe-M <moshopjadi1602@gmail.com>
2022-09-12 19:41:59 -0700

    low level programming

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -14,14 +14,13 @@ int main(void)
 {
 	char letter;
 
-	for (letter= 'a'; letter <= 'z'; letter++)
-	  putchar(letter);
-	
-	for (letter= 'A'; letter <= 'Z'; letter++)
-	  putchar(letter);
+	for (letter = 'a'; letter <= 'z'; letter++)
+		putchar(letter);
+
+	for (letter = 'A'; letter <= 'Z'; letter++)
+		putchar(letter);
 
 	putchar('\n');
 
 	return (0);
-
 }
