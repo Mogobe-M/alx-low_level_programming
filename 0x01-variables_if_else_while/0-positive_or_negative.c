e64514c58550c8bac784c3acbd50d767fb0edace
Mogobe-M <moshopjadi1602@gmail.com>
2022-09-15 08:19:16 -0700

    0-positive_or_negative.c

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,6 +1,6 @@
 /**
- *File: 0-positive_or_negative.c
- *Auth: Mogobe Monoge
+*File: 0-positive_or_negative.c
+*Auth: Mogobe Monoge
  **/
 
 #include <stdio.h>
@@ -8,11 +8,11 @@
 #include <time.h>
 
 /**
-*main- This program will assign a random number to the variable n each time it 
+ *main- This program will assign a random number to the variable n each time it
 is executed. Complete the source code in order to print whether the number
  stored in the variable n is positive or negative.
-*discription: using the main function
-*return 0
+ *discription: using the main function
+ *return 0
  **/
 
 int main(void)
